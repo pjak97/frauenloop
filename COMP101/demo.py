162e95eeed84142e84e9e09d0a952f0b17ea4e3e
priyanka <priyanka.jakhautia@gmail.com>
2023-02-01 18:46:12 +0100

    demo file

---
+++ COMP101/demo.py
@@ -0,0 +1,18 @@
+#
+msg = 'hello'
+print(msg)
+
+my_name ='priyanka'
+print(my_name)
+
+my_age = 27
+print(my_age)
+
+temp = 2.2
+print(temp)
+
+#calculate the year I was born:
+current_year = 2022
+my_birthyear =  current_year - my_age
+
+print(my_birthyear)
